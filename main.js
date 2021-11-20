c80d24e37e115852cb83287f137c652834297768
Anthony Ciocco <anthonymciocco@gmail.com>
2021-11-20 16:29:55 -0600

    Better presentation of issues

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 async function funcTest(){
-     document.getElementById("bruh2").innerHTML = "YOU CLICKED ME!";
+     document.getElementById("bruh").innerHTML = "YOU CLICKED ME!";
+      document.getElementById("bruh2").innerHTML = "YOU CLICKED ME!";
 }
